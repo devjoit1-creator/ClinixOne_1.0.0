78dab6fa093e0832ca7f83ca603c6b436bc1f176
devjoit1-creator <dev.joit1@gmail.com>
2026-01-21 12:27:06 -0500

    Visualizacion de atenciones de consulta

--- app/static/js/epicrisis/add_epicrisis.js
+++ app/static/js/epicrisis/add_epicrisis.js
@@ -1,12 +1,21 @@
-/* Constantes */
+/* Constantes Formulario */
 const $tipo_doc = document.getElementById("tipo_doc");
 const $codigo = document.getElementById("codigo");
 const $paciente = document.getElementById("paciente");
 const $medico = document.getElementById("medico");
-const $tablaBusquedaPacientesEpicrisis = document.getElementById("tablaBusquedaPacientesEpicrisis");
-const $modal_resultadoAtenciones = document.getElementById("modal_resultadoAtenciones");
 const $btn_consultar = document.getElementById("btn_consultar");
 const $btn_cancelar = document.getElementById("btn_cancelar");
+
+/* Constantes Modal Pacientes */
+const $tablaBusquedaPacientesEpicrisis = document.getElementById("tablaBusquedaPacientesEpicrisis");
+
+/* Constantes Modal Atenciones */
+const $modal_resultadoAtenciones = document.getElementById("modal_resultadoAtenciones");
+const $tab_consultas = document.getElementById("tab_consultas");
+const $tabAtencionesConsultas = document.getElementById("tabAtencionesConsultas");
+const $tab_hosp = document.getElementById("tab_hosp");
+const $tabAtencionesHosp = document.getElementById("tabAtencionesHosp");
+const $tablaAtencionesConsultaEpicrisis = document.getElementById("tablaAtencionesConsultaEpicrisis");
 
 /* Obtener Datos de paciente desde el modal pacientes */
 $tablaBusquedaPacientesEpicrisis.addEventListener("click", (e) => {
@@ -28,7 +37,8 @@ const validar = () => {
             icon: "warning"
         });
     } else {
-       getAtencionesConsultaEpicrisis();
+        activarModalResultados();
+        getAtencionesConsultaEpicrisis();
     }
 }
 
@@ -41,6 +51,11 @@ $btn_consultar.addEventListener("click", (e) => {
 const getAtencionesConsultaEpicrisis = () => {
     let paciente = $codigo.value;
     let medico = $medico.value;
+    /* Limpiar Tabla */
+    while($tablaAtencionesConsultaEpicrisis.rows.length > 1){
+        $tablaAtencionesConsultaEpicrisis.deleteRow(1);
+    }
+    /* API Fetch */
     fetch("/getAtencionesConsultaEpicrisis", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
@@ -48,6 +63,7 @@ const getAtencionesConsultaEpicrisis = () => {
     })
     .then(response => response.json())
     .then(data => {
+        /* Alerta si no hay resultados */
         if(!Array.isArray(data) || data.length === 0){
             Swal.fire({
                 title: "Advertencia!",
@@ -56,12 +72,43 @@ const getAtencionesConsultaEpicrisis = () => {
             });
             return;
         }
-        
+        /* Resultados en tabla */
         data.forEach(atencion => {
-            alert(`la atención es: ${atencion.atencion}, ingresó ${atencion.ingreso}, salió ${atencion.salida}, en el servicio ${atencion.servicio}`)
+            $tablaAtencionesConsultaEpicrisis.insertRow().innerHTML = `
+                <td style="width: 20%; font-size: x-small;">${atencion.atencion}</td>
+                <td style="width: 20%; font-size: x-small;">${atencion.ingreso}</td>
+                <td style="width: 20%; font-size: x-small;">${atencion.salida}</td>
+                <td style="width: 20%; font-size: x-small;">${atencion.servicio}</td>
+            `
         });
     })
     .catch(error => console.error("error: ", error))
+}
+
+/* Activar Modal de Resultados de Atenciones */
+const activarModalResultados = () => {
+    $modal_resultadoAtenciones.classList.remove("is-hidden");
+    $modal_resultadoAtenciones.classList.add("is-active");
+}
+
+/* Activar/Desactivar Pestañas */
+$tab_consultas.addEventListener("click", () => {
+    ocultarCont();
+    $tab_consultas.classList.add("is-active");
+    $tabAtencionesConsultas.classList.remove("is-hidden");
+    $tab_hosp.classList.remove("is-active")
+})
+
+$tab_hosp.addEventListener("click", () => {
+    ocultarCont();
+    $tab_hosp.classList.add("is-active");
+    $tabAtencionesHosp.classList.remove("is-hidden");
+    $tab_consultas.classList.remove("is-active");
+})
+
+function ocultarCont(){
+    $tabAtencionesConsultas.classList.add("is-hidden");
+    $tabAtencionesHosp.classList.add("is-hidden");
 }
 
 /* Modo Cancelar */
